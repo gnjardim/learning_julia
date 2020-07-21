5902b6baf5a8aaf39da23b0ea00d5b423c3475cc
gnjardim <48603966+gnjardim@users.noreply.github.com>
2020-07-21 16:04:19 -0300

    add first steps

---
+++ 0_first_steps.jl
@@ -0,0 +1,12 @@
+# Use "Add Plots" in pkg> (press `]`) to install package. Press backspace to return to julia>
+using Plots
+
+# get help from function
+?repeat
+?replace
+?plot
+
+# plot sin
+plot(sin, -2π, 2π, label="sin(x)")
+
+# Check packages in current session "] st"
